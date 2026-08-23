9063d33d54fc9a21987542e1db27f1a5eac10123
Aditya Thakur <165558592+aditya0563@users.noreply.github.com>
2026-08-23 16:10:47 +0530

    Time: 0 ms (100%), Space: 12.9 MB (95.6%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void helper(int n,int open,int close,int idx,string& temp, vector<string>& ans){
-        if(idx==2*n){
-            ans.push_back(temp);
+void backtrack(int openN, int closedN, int n, string& current, vector<string>& res) {
+        if(openN == n && closedN == n) {
+            res.push_back(current);
             return;
         }
-
-        if(open<n){
-            temp[idx]='(';
-            helper(n,open+1,close,idx+1,temp,ans);
+        if(openN < n) {
+            current.push_back('(');
+            backtrack(openN + 1, closedN, n, current, res);
+            current.pop_back();
         }
-
-        if(close<open){
-            temp[idx]=')';
-            helper(n,open,close+1,idx+1,temp,ans);
+        if(closedN < openN) {
+            current.push_back(')');
+            backtrack(openN, closedN + 1, n, current, res);
+            current.pop_back();
         }
     }
-
+    
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        string temp(2*n,' ');
-
-        helper(n,0,0,0,temp,ans);
-
-        return ans;
+        vector<string> res;
+        string current = "";
+        backtrack(0, 0, n, current, res);
+        return res;
     }
 };
