0812c705ee21951e5af0615ad4e627a1f4bb2cf7
Aditya Thakur <165558592+aditya0563@users.noreply.github.com>
2026-09-06 10:25:46 +0530

    Time: 293 ms (37.67%), Space: 66.8 MB (58.82%) - LeetHub

--- 2882-drop-duplicate-rows/2882-drop-duplicate-rows.py
+++ 2882-drop-duplicate-rows/2882-drop-duplicate-rows.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    df=customers.drop_duplicates(subset=['email'])
-    return df
+    return customers.drop_duplicates(subset=['email'])
